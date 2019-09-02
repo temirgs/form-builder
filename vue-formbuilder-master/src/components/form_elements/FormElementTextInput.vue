f158d1387987adde95b4a6d7fe37eed9c0a4a895
temir_gs <gasimov.temir@gamil.com>
2019-09-02 04:14:44 +0400

    bjdbjs

---
+++ vue-formbuilder-master/src/components/form_elements/FormElementTextInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <el-input :placeholder="currentField.isPlaceholderVisible ? currentField.placeholder : ''"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextInput",
+  props: ["currentField"]
+};
+</script>
